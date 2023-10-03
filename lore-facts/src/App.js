254be5e8415fde27058eff4d8041e753a76b00d2
ScorpN <sf_commander@outlook.com>
2023-10-04 00:11:39 +0300

    The content of the p tag at line 44 was updated by clicking the images.

--- lore-facts/src/App.js
+++ lore-facts/src/App.js
@@ -1,38 +1,47 @@
 import  { lore_object }  from './data/characters';
-
-
-const title = '';
-const loreFacts = <h1>{title === '' ? 'Click it' : ''}</h1>;
-
-const handleClick = ({target}) => {
-  console.log(target.alt);  
-}
-
-
-const charaters = (
-    Object.keys(lore_object).map((item ,i) => {
-      return (
-        <img 
-        src={lore_object[item].img} 
-        onClick={handleClick}
-        key={item}
-        alt={item}
-        aria-label={item}
-        />
-      ) 
-    })
-);
-
-
-
+import { useState } from 'react';
 
 export default function App() {
+
+  const [lore, setLore] = useState();
+
+  const title = '';
+  const loreFacts = <h1>{title === '' ? 'Click it' : ''}</h1>;
+
+
+
+  const handleClick = ({target}) => {
+    Object.keys(lore_object).map((item, i) => {
+      /*setLore(target.alt === item ? lore_object[item].lore : 'null')*/
+      if(target.alt === item){
+        setLore(lore_object[item].lore);
+      }
+    })
+    console.log(lore); /*....jsx degeri güncellenirken consol'da delay var....*/
+  }
+
+
+  const charaters = (
+      Object.keys(lore_object).map((item ,i) => {
+        return (
+          <img 
+            src={lore_object[item].img} 
+            onClick={handleClick}
+            key={item}
+            alt={item}
+            aria-label={item}
+          />
+        ) 
+      })
+  );
+
+
   return (
     <>
       {loreFacts}
       <div className = 'lore' style={{backgroundImage: 'url(./images/land.jpg)'}}>
           <div className = 'lore-text'>
-         
+            <p>{lore}</p>
           </div>
           <div className = 'lore-chars'>
             {charaters}
