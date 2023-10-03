83b2c6783a214c3d5feb84ec4e695cdea49888a6
ScorpN <sf_commander@outlook.com>
2023-10-03 14:49:36 +0300

    Title added. get alt of image by clicking.

--- lore-facts/src/App.js
+++ lore-facts/src/App.js
@@ -1,31 +1,44 @@
 import  { lore_object }  from './data/characters';
 
-const element = (
-  <div>    
-  </div>
-)
+
+const title = '';
+const loreFacts = <h1>{title === '' ? 'Click it' : ''}</h1>;
+
+const handleClick = ({target}) => {
+  console.log(target.alt);  
+}
+
 
 const charaters = (
     Object.keys(lore_object).map((item ,i) => {
       return (
         <img 
         src={lore_object[item].img} 
+        onClick={handleClick}
+        key={item}
+        alt={item}
+        aria-label={item}
         />
       ) 
     })
 );
 
 
+
+
 export default function App() {
   return (
-    <div className = 'lore' style={{backgroundImage: 'url(./images/land.jpg)'}}>
-        <div className = 'lore-text'>
-        test
-        </div>
-        <div className = 'lore-chars'>
-          {charaters}
-        </div>
-    </div>
+    <>
+      {loreFacts}
+      <div className = 'lore' style={{backgroundImage: 'url(./images/land.jpg)'}}>
+          <div className = 'lore-text'>
+         
+          </div>
+          <div className = 'lore-chars'>
+            {charaters}
+          </div>
+      </div>
+    </>
   );
 }
 
