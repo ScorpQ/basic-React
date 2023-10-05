7c640038109a782e5754031d1fbfb383e7094f5a
ScorpN <sf_commander@outlook.com>
2023-10-05 13:12:02 +0300

    The contact section has been prepared and scss added.

--- auth-form/src/App.js
+++ auth-form/src/App.js
@@ -1,7 +1,13 @@
+import './style.css';
+
 function App() {
   return (
-    <div className="App">
-     ASDFA
+    <div className = "contact">
+      <h1>Contact</h1>
+      <div className = "contact-info">
+        <p>client@example.com</p>
+        <p>555.555.5555</p>
+      </div>
     </div>
   );
 }
