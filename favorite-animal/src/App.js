e0ef600376074c6062be27628bc648c7401bba22
ScorpN <sf_commander@outlook.com>
2023-10-23 10:17:40 +0300

    API data assigned to the variable via useEffect.

--- favorite-animal/src/App.js
+++ favorite-animal/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  
+  const[data, setData] = useState();
+
   const getData = async () => {
     try{
       const response = await fetch('https://jsonplaceholder.typicode.com/posts')
@@ -11,12 +12,25 @@ function App() {
     }
     catch(error){
       console.log(error);
-      console.log(error);
     }
   }
 
+  useEffect(() => {
+    async function getDatas(){
+      try {
+        const jsonData = await getData();
+        console.log(jsonData);
+      }catch(error){
+        console.log(error);
+      }
+    }
+    getDatas();
+  },[]);
+
   return (
     <div className="App">
+   
+
     </div>
   );
 }
