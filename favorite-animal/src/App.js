ab35140718ca588c0fd460cc2b905f43382f26f5
ScorpN <sf_commander@outlook.com>
2023-10-27 10:11:31 +0300

    Options storage as jsx.

--- favorite-animal/src/App.js
+++ favorite-animal/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const[data, setData] = useState();
   const[url, setUrl] = useState();
 
+  // fetch json and set it to hook names 'data'.
   useEffect(() => {
     async function getData(){
       try {
@@ -20,23 +21,34 @@ function App() {
     getData();
   },[]);
 
+  // image set to url from state setter hook named data.
   useEffect(() => {
     let counter = 0;
     data && setUrl(data[counter].url)
     const interval = setInterval(() => {
       setUrl(data[counter].url)
       counter === data.length - 1 ? counter = 0 : counter++ ;
-    }, 2000)
+    }, 1000)
 
-    
     return () => {
       clearInterval(interval);
     }
   }, [data])  
 
+  const myElement = (
+    data && data.map((item) => {
+      return <option key={item.id}> {item.title} </option>
+      })
+    );
+
+
+
   return (
     <div className="App">
-      <img src={url} />
+        <img src={url} />
+        <select name="colors" id="colors">
+          { myElement }
+        </select>
     </div>
   );
 }
--- favorite-animal/src/api/index.js
+++ favorite-animal/src/api/index.js
@@ -1,7 +1,7 @@
  const getApiData = async () => {
     try{
       const response = await fetch('https://jsonplaceholder.typicode.com/photos')
-      const jsonResponse = await response.ok ? await response.json() : null;
+      const jsonResponse = response.ok ? response.json() : null;
       return jsonResponse;
     }
     catch(error){
