cbc1e1a2f90191820d926d981f6ccb76887171bc
ScorpN <sf_commander@outlook.com>
2023-11-01 17:28:03 +0300

    Understanding testing.

---
+++ first-testing/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import link from './link'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
