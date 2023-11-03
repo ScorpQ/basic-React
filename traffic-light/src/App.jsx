fafe73adf927638ad8b01d971fcdaaf971c4d485
ScorpN <sf_commander@outlook.com>
2023-11-04 00:18:16 +0300

    Traffic-light solved

--- traffic-light/src/App.jsx
+++ traffic-light/src/App.jsx
@@ -4,12 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [active, setActive] = useState();
+
+  const test = ({target}) => {
+    setActive( (target.id == 1) ? 'red' : (target.id == 2) ? 'yellow' : 'green')
+  }
+
   return (
     <>
       <div className='wrapper'>
-        <button></button>
-        <button></button>
-        <button></button>
+        <button id="1" onClick={test} className={active == 'red' ? "red" : ""}></button>
+        <button id="2" onClick={test} className={active == 'yellow' ? "yellow" : ""}></button>
+        <button id="3" onClick={test} className={active == 'green' ? "green" : ""}></button>
       </div>
     </>
   )
